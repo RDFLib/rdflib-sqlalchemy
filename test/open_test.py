0022bb4ddc5f213b14734a16a1e096ef59a83eb5
Mark Watts <watts.mark2015@gmail.com>
2021-10-23 18:25:55 -0500

    Changing expected error for test_open_corrupted

--- test/open_test.py
+++ test/open_test.py
@@ -1,4 +1,5 @@
 import unittest
+import sqlite3
 
 from rdflib import plugin
 from rdflib.graph import Graph, Store
@@ -8,5 +9,5 @@ class TestOpen(unittest.TestCase):
     def test_open_corrupted(self):
         store = plugin.get('SQLAlchemy', Store)(identifier='open_test')
         self.graph = Graph(store, identifier='open_test')
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(sqlite3.OperationalError):
             self.graph.open("sqlite:///test/corrupted.sqlite", create=False)
