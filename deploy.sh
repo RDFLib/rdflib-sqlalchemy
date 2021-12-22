db29c67a8a7d8978b7f74603e88371955f5c7f86
Mark Watts <watts.mark2015@gmail.com>
2021-12-22 10:03:37 -0600

    Correcting deploy.sh check for version number change

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git log --format=%s -n 1 "$HEAD_REV" | grep -q '^MINOR:' && exit 0
 
 date=$(date +"%Y%m%d%H%M%S")
 sed -i -r 's/^version = "([^"]+)(\.dev|a)0"$/version = "\1\2'$date'"/' setup.py
-git diff -q setup.py && echo "Unsupported version format (release?)" && exit 0
+git diff --quiet setup.py && echo "Unsupported version format (release?)" && exit 0
 python setup.py egg_info sdist bdist_wheel
 twine upload -c "Built by CI. Uploaded after $(date +"%Y-%m-%d %H:%M:%S")" dist/rdflib-sqlalchemy*tar.gz dist/rdflib_sqlalchemy*whl
