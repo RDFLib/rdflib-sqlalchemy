e0ea83b31e3365f5539bef9f3c8354bc424efbdf
Alexandre Detiste <alexandre.detiste@gmail.com>
2025-06-16 02:05:17 +0200

    remove conditional usage of old external "mock", already removed from setup.py

--- test/test_sqlalchemy.py
+++ test/test_sqlalchemy.py
@@ -1,9 +1,5 @@
 import unittest
-
-try:
-    from unittest.mock import patch, MagicMock
-except ImportError:
-    from mock import patch, MagicMock
+from unittest.mock import patch, MagicMock
 
 from rdflib import (
     ConjunctiveGraph,
